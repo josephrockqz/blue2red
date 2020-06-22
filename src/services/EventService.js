28a93d1a45a0b03ee4f5d6a424259e010a0e99a3
Joseph Rock <jhr4qz@virginia.edu>
2020-06-21 22:59:33 -0500

    created new txt file for saving useful commands, so far have three commands relating to the backend mongodb server/database

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
 
+// const apiClient = axios.create({
+//   baseURL: `http://localhost:3000`,
+//   withCredentials: false,
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   timeout: 10000
+// })
+
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
-  withCredentials: false,
+  baseURL: `https://18.188.54.151:3000`,
+  // withCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
