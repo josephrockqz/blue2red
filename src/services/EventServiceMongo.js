e709362537e939270ee6615b821762bd31679e05
Joseph Rock <jhr4qz@virginia.edu>
2020-06-24 23:59:11 -0500

    commented out vue config file, it was messing stuff up

--- src/services/EventServiceMongo.js
+++ src/services/EventServiceMongo.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = '/api/posts'
+const url = 'http://node.joerock.dev/api/posts'
 
 class EventServiceMongo {
   // Get Scores
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 // const path = require('path')
 
-module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://node.joerock.dev' // 18.188.54.151, localhost:5000, node.joerock.dev
-      }
-    }
-  }
-}
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://node.joerock.dev' // 18.188.54.151, localhost:5000, node.joerock.dev
+//       }
+//     }
+//   }
+// }
